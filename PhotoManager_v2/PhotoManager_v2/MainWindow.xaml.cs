b9ddf7d536a33c75eaa4745f99f929e00d5b37ba
Marcin filapek <filap4@gmail.com>
2019-03-28 14:43:54 +0100

    Wprowadznie rensponsywnego layoutu i dodanie przycisków do dodawania zdjęć do slidera i otwierania programu

--- PhotoManager_v2/PhotoManager_v2/MainWindow.xaml.cs
+++ PhotoManager_v2/PhotoManager_v2/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using Path = System.IO.Path;
 
 namespace PhotoManager_v2
 {
@@ -23,6 +17,56 @@ namespace PhotoManager_v2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddPhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            Image image = new Image();
+            Label label = new Label();
+
+
+            openFileDialog.InitialDirectory = @"C:\";
+            openFileDialog.Filter = "JPEG (*.jpg)|*.jpg|PNG (*.png)|*.png|TIFF (*.tiff)|*.tiff|All Files (*.*)|*.*"; ;
+            openFileDialog.Multiselect = true;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            image.HorizontalAlignment = HorizontalAlignment.Center;
+            image.MaxHeight = 150;
+            image.MinHeight = 60;
+            image.MaxWidth = 150;
+            image.MinWidth = 60;
+            image.Stretch = Stretch.Uniform;
+
+
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+                    image.Source = new BitmapImage(new Uri(filename));
+                    label.Content = Path.GetFileName(filename);
+                    Slider.Children.Add(image);
+                    Slider.Children.Add(label);
+                }
+            }
+        }
+
+        private  void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShowInTaskbar = false;
+            Hide();
+
+            ProcessStartInfo start_info = new ProcessStartInfo("wordpad.exe");      //jak ustawić otwieranie innego programu bez błędu              //ProcessStartInfo(Scieżka do otwieranego programu, plik który otwieramy od razu w programie)
+            Process process = new Process();
+
+            start_info.WindowStyle = ProcessWindowStyle.Maximized;
+            process.StartInfo = start_info;
+            process.Start();
+            process.WaitForExit();
+            ShowInTaskbar = true;
+            Show();
         }
     }
 }

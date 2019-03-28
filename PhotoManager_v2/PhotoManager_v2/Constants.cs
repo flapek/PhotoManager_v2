1478a01b5ad5eeccbe173724a57936b15e80175e
Marcin filapek <filap4@gmail.com>
2019-03-28 19:15:26 +0100

    Add directory tree

---
+++ PhotoManager_v2/PhotoManager_v2/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoManager_v2
+{
+    class Constants
+    {
+        public const string ErrorProgramFileNameIsFail = "File name or path is wrong!"; 
+
+    }
+}

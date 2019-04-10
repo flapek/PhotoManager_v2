eae429fdb2100a50c00df3639b10f017db9ebb3c
Marcin filapek <filap4@gmail.com>
2019-04-10 14:26:07 +0200

    usunięcie niepotrzebnego kodu

--- PhotoManager_v2/PhotoManager_v2/PhotoManager_MainWindow.xaml.cs
+++ PhotoManager_v2/PhotoManager_v2/PhotoManager_MainWindow.xaml.cs
@@ -15,7 +15,6 @@ namespace PhotoManager_v2
         public MainWindow()
         {
             InitializeComponent();
-            //LoadDirectories();
             tree.LoadDirectories(@"C:\Users\filap\Desktop", DirectoryTreeView);       //błąd wywołania
         }
         void MainWindow_Closing(object sender, CancelEventArgs e)
@@ -47,7 +46,6 @@ namespace PhotoManager_v2
                     slider.AddElement(filename, Slider);
                 }
             }
-
         }
         private async void EditInOtherProgramMenuItem_ClickAsync(object sender, RoutedEventArgs e)
         {
@@ -59,142 +57,5 @@ namespace PhotoManager_v2
             option.Show();
         }
 
-
-
-
-        /*------------------------------------------------------------------------------------------------------------------------*/
-        /*                                              Przenieść do osobnej klasy                                              */
-        //private void LoadDirectories()
-        //{
-        //    DriveInfo[] drives = DriveInfo.GetDrives();
-        //    foreach (var drive in drives)
-        //    {
-        //        DirectoryTreeView.Items.Add(GetItem(drive));
-        //    }
-        //}
-        //private TreeViewItem GetItem(DriveInfo drive)
-        //{
-        //    var item = new TreeViewItem
-        //    {
-        //        Header = drive.Name,
-        //        DataContext = drive,
-        //        Tag = drive
-        //    };
-        //    AddDummy(item);
-        //    item.Expanded += new RoutedEventHandler(item_Expanded);
-        //    return item;
-        //}
-        //public class DummyTreeViewItem : TreeViewItem
-        //{
-        //    public DummyTreeViewItem()
-        //        : base()
-        //    {
-        //        Header = "Dummy";
-        //        Tag = "Dummy";
-        //    }
-        //}
-        //private TreeViewItem GetItem(DirectoryInfo directory)
-        //{
-        //    var item = new TreeViewItem
-        //    {
-        //        Header = directory.Name,
-        //        DataContext = directory,
-        //        Tag = directory
-        //    };
-        //    this.AddDummy(item);
-        //    item.Expanded += new RoutedEventHandler(item_Expanded);
-        //    return item;
-        //}
-        //private TreeViewItem GetItem(FileInfo file)
-        //{
-        //    var item = new TreeViewItem
-        //    {
-        //        Header = file.Name,
-        //        DataContext = file,
-        //        Tag = file
-        //    };
-        //    return item;
-        //}
-        //private void AddDummy(TreeViewItem item)
-        //{
-        //    item.Items.Add(new DummyTreeViewItem());
-        //}
-        //private bool HasDummy(TreeViewItem item)
-        //{
-        //    return item.HasItems && (item.Items.OfType<TreeViewItem>().ToList().FindAll(tvi => tvi is DummyTreeViewItem).Count > 0);
-        //}
-        //private void RemoveDummy(TreeViewItem item)
-        //{
-        //    var dummies = item.Items.OfType<TreeViewItem>().ToList().FindAll(tvi => tvi is DummyTreeViewItem);
-        //    foreach (var dummy in dummies)
-        //    {
-        //        item.Items.Remove(dummy);
-        //    }
-        //}
-        //private void ExploreDirectories(TreeViewItem item)
-        //{
-        //    var directoryInfo = (DirectoryInfo)null;
-        //    if (item.Tag is DriveInfo)
-        //    {
-        //        directoryInfo = ((DriveInfo)item.Tag).RootDirectory;
-        //    }
-        //    else if (item.Tag is DirectoryInfo)
-        //    {
-        //        directoryInfo = (DirectoryInfo)item.Tag;
-        //    }
-        //    else if (item.Tag is FileInfo)
-        //    {
-        //        directoryInfo = ((FileInfo)item.Tag).Directory;
-        //    }
-        //    if (ReferenceEquals(directoryInfo, null)) return;
-        //    foreach (var directory in directoryInfo.GetDirectories())
-        //    {
-        //        var isHidden = (directory.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
-        //        var isSystem = (directory.Attributes & FileAttributes.System) == FileAttributes.System;
-        //        if (!isHidden && !isSystem)
-        //        {
-        //            item.Items.Add(GetItem(directory));
-        //        }
-        //    }
-        //}
-        //private void ExploreFiles(TreeViewItem item)
-        //{
-        //    var directoryInfo = (DirectoryInfo)null;
-        //    if (item.Tag is DriveInfo)
-        //    {
-        //        directoryInfo = ((DriveInfo)item.Tag).RootDirectory;
-        //    }
-        //    else if (item.Tag is DirectoryInfo)
-        //    {
-        //        directoryInfo = (DirectoryInfo)item.Tag;
-        //    }
-        //    else if (item.Tag is FileInfo)
-        //    {
-        //        directoryInfo = ((FileInfo)item.Tag).Directory;
-        //    }
-        //    if (ReferenceEquals(directoryInfo, null)) return;
-        //    foreach (var file in directoryInfo.GetFiles())
-        //    {
-        //        var isHidden = (file.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
-        //        var isSystem = (file.Attributes & FileAttributes.System) == FileAttributes.System;
-        //        if (!isHidden && !isSystem)
-        //        {
-        //            item.Items.Add(this.GetItem(file));
-        //        }
-        //    }
-        //}
-        //void item_Expanded(object sender, RoutedEventArgs e)
-        //{
-        //    var item = (TreeViewItem)sender;
-        //    if (HasDummy(item))
-        //    {
-        //        Cursor = Cursors.Wait;
-        //        RemoveDummy(item);
-        //        ExploreDirectories(item);
-        //        ExploreFiles(item);
-        //        Cursor = Cursors.Arrow;
-        //    }
-        //}
-        /*------------------------------------------------------------------------------------------------------------------------*/
     }
 }
